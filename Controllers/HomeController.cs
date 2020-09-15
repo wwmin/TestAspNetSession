039538dc5826bdadb8ca5e6f866ad105de4c9cc9
wwmin <wwei.min@163.com>
2020-09-15 14:53:27 +0800

    feat: Session产生条件

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,20 +10,19 @@ namespace TestSessionWeb.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.SessionId = Session.SessionID;
+            HttpContext.Cache.Insert("admin", Session.SessionID, null, DateTime.Now.AddSeconds(10), TimeSpan.Zero);
+            Session["admin"] = "admin";//当使用Session保存一些信息的时候才会在浏览器中生成cookie保存sessionId, 可以取消此处尝试
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.SessionId = Session.SessionID;
+            var _cache = HttpContext.Cache.Get("admin");
+            ViewBag.Admin = _cache;
+            ViewBag.SessionInfoAdmin = Session["admin"];
             return View();
         }
     }
